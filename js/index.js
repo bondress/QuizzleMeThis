c3357948ae743b0772c811e3c0b662e1d5918d17
Laide Endeley <endeleylaide@gmail.com>
2024-01-18 06:29:19 +0100

    Added back to top button

--- js/index.js
+++ js/index.js
@@ -24,3 +24,23 @@ let year = d.getFullYear();
 
 footYear = document.querySelector(".current-year");
 footYear.innerHTML = year;
+
+let topButton = document.querySelector(".top-btn");
+
+window.onscroll = function() {
+    scrollFunction()
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 ||
+        document.documentElement.scrollTop > 20 ) {
+            topButton.style.display = "block";
+        } else {
+            topButton.style.display = "none";
+        }
+}
+
+function backToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
